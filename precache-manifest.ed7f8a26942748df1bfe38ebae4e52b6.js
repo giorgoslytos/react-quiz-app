3b9370c277ed79cf961d7cdbf2d5ca5b5e750bf4
giorgoslytos <giorgoslytos@gmail.com>
2020-07-18 18:46:24 +0300

    Updates

---
+++ precache-manifest.ed7f8a26942748df1bfe38ebae4e52b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cbff2dff86dd35797381a569710726",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "3b748726705185d70b7d",
+    "url": "/react-quiz-app/static/css/main.26284cd5.chunk.css"
+  },
+  {
+    "revision": "ba97cfa48d329214c2cb",
+    "url": "/react-quiz-app/static/js/2.da1ecf1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-app/static/js/2.da1ecf1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b748726705185d70b7d",
+    "url": "/react-quiz-app/static/js/main.a94a91f0.chunk.js"
+  },
+  {
+    "revision": "5f9ebe21e9892c6e5b70",
+    "url": "/react-quiz-app/static/js/runtime-main.1ae03102.js"
+  }
+]);
